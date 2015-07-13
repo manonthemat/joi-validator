02f9fef6596672a1e621809800e2f424b1cf6b27
manonthemat <matthiasksieber@gmail.com>
2015-07-13 15:24:55 -0700

    Regex changes
    - defaultRegex accepts now all characters, but escapes escapes and double quotes
    - passwordRegex see above
    - titleRegex and messageRegex removed

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ var Joi = require('joi');
 
 module.exports = (function() {
   return {
-    defaultRegex: Joi.string().regex(/[^\\]/).replace('"', '\\"'), // TODO: make replace easily chainable, so escapes get escaped, too
-    passwordRegex: Joi.string().regex(/^[^\\"]+$/).required(),
+    defaultRegex: Joi.string().replace('\\', '\\\\').replace('"', '\\"'),
+    passwordRegex: Joi.string().replace('\\', '\\\\').replace('"', '\\"'),
     slugRegex: Joi.string().regex(/^[a-z0-9-]+$/i),
     searchRegex: Joi.string().regex(/[^\\]/).replace('"', '\\"'),
     skuRegex: Joi.string().regex(/^[\w|-]+$/),
-    titleRegex: Joi.string().regex(/[^\\]/).replace('"', '\\"'),
-    messageRegex: Joi.string().regex(/[^\\]/).replace('"', '\\"'),
     mediaUrlRegex: Joi.string().uri({ scheme: ['http', 'https'] }),
     statesRegex: Joi.string().regex(/(^AL$)|(^AK$)|(^AZ$)|(^AR$)|(^CA$)|(^CO$)|(^CT$)|(^DE$)|(^FL$)|(^GA$)|(^HI$)|(^ID$)|(^IL$)|(^IN$)|(^IA$)|(^KS$)|(^KY$)|(^LA$)|(^ME$)|(^MD$)|(^MA$)|(^MI$)|(^MN$)|(^MS$)|(^MO$)|(^MT$)|(^NE$)|(^NV$)|(^NH$)|(^NJ$)|(^NM$)|(^NY$)|(^NC$)|(^ND$)|(^OH$)|(^OK$)|(^OR$)|(^PA$)|(^RI$)|(^SC$)|(^SD$)|(^TN$)|(^TX$)|(^UT$)|(^VT$)|(^VA$)|(^WA$)|(^WV$)|(^WI$)|(^WY$)/i),
     hexColorRegex: Joi.string().regex(/^[a-f0-9]{6}$/),
